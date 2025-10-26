961becd5c4b68065d0a275303923529661ca6570
XuHaoRan <no_signal@sjtu.edu.cn>
2025-10-26 15:37:12 +0800

    feat: 完善了获取连接状态的接口函数，归一化摇杆量，给开关按键状态设置为枚举，更加清晰

--- User/RemoteController.h
+++ User/RemoteController.h
@@ -6,29 +6,36 @@
 #define REMOTE_CONTROL_REMOTECONTROLLER_H
 #include "stm32f4xx_hal.h"
 
+enum Switch { UP = 0, MID, DOWN };
+
 class RemoteController {
 private:
     static constexpr size_t RX_DMA_BUFFER_SIZE = 512;
     static constexpr size_t FRAME_SIZE = 18;
+    static constexpr uint32_t TIMEOUT = 100; // 超时时间 ms
 
     uint8_t rx_dma_buffer_[RX_DMA_BUFFER_SIZE]{ 0 };
     uint8_t rx_data_[FRAME_SIZE]{ 0 };
     UART_HandleTypeDef* huart_;
-    bool isConnected = false; // 连接状态
+    bool isConnected_ = false; // 连接状态
     uint32_t last_rx_tick_ = 0; // 上次接收数据的tick
 
     struct Data {
-        uint16_t ch0 = 0, ch1 = 0, ch2 = 0, ch3 = 0;
-        uint8_t s1 = 0, s2 = 0;
+        float ch0 = 0.0f, ch1 = 0.0f, ch2 = 0.0f, ch3 = 0.0f;
+        Switch s1 = UP, s2 = UP;
         int16_t mouse_x = 0, mouse_y = 0, mouse_z = 0;
         uint8_t mouse_press_l = 0, mouse_press_r = 0;
         uint16_t key = 0;
     } data_;
 
+    static float normalize_channel(int16_t value) {
+        return (static_cast<float>(value) - 1024.0f) / 660.0f;
+    }
 public:
     void init(UART_HandleTypeDef* huart);
     void handle(uint16_t Size);
-    bool& connect_state();
+    bool connect_state();
+
 };
 
 
--- User/callback.cpp
+++ User/callback.cpp
@@ -11,7 +11,6 @@ extern RemoteController remoteController;
 void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size) {
     if (huart == &huart3) {
         // 处理接收到的数据
-        remoteController.connect_state() = true;
         remoteController.handle(Size);
     }
 }
