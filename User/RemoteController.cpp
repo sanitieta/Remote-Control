2ee134cd543f277ad0e2cf704c19879d323af5a7
XuHaoRan <no_signal@sjtu.edu.cn>
2025-10-26 15:37:12 +0800

    feat: 完善了获取连接状态的接口函数，归一化摇杆量，给开关按键状态设置为枚举，更加清晰

--- User/RemoteController.cpp
+++ User/RemoteController.cpp
@@ -16,15 +16,20 @@ void RemoteController::init(UART_HandleTypeDef* huart) {
 void RemoteController::handle(uint16_t Size) {
     if (Size != FRAME_SIZE) { return; }
     memcpy(rx_data_, rx_dma_buffer_, FRAME_SIZE);
+    last_rx_tick_ = HAL_GetTick(); // 更新最后接收时间戳
 
     // 解包数据
-    data_.ch0 = (static_cast<int16_t>(rx_data_[0]) | (static_cast<int16_t>(rx_data_[1]) << 8)) & 0x07FF;
-    data_.ch1 = ((static_cast<int16_t>(rx_data_[1]) >> 3) | (static_cast<int16_t>(rx_data_[2]) << 5)) & 0x07FF;
-    data_.ch2 = ((static_cast<int16_t>(rx_data_[2]) >> 6) | (static_cast<int16_t>(rx_data_[3]) << 2) | (static_cast<
-        int16_t>(rx_data_[4]) << 10)) & 0x07FF;
-    data_.ch3 = ((static_cast<int16_t>(rx_data_[4]) >> 1) | (static_cast<int16_t>(rx_data_[5]) << 7)) & 0x07FF;
-    data_.s1 = ((rx_data_[5] >> 4) & 0x000C) >> 2;
-    data_.s2 = ((rx_data_[5] >> 4) & 0x0003);
+    data_.ch0 = normalize_channel(
+        (static_cast<int16_t>(rx_data_[0]) | (static_cast<int16_t>(rx_data_[1]) << 8)) & 0x07FF);
+    data_.ch1 = normalize_channel(
+        ((static_cast<int16_t>(rx_data_[1]) >> 3) | (static_cast<int16_t>(rx_data_[2]) << 5)) & 0x07FF);
+    data_.ch2 = normalize_channel(
+        ((static_cast<int16_t>(rx_data_[2]) >> 6) | (static_cast<int16_t>(rx_data_[3]) << 2) | (static_cast<
+            int16_t>(rx_data_[4]) << 10)) & 0x07FF);
+    data_.ch3 = normalize_channel(
+        ((static_cast<int16_t>(rx_data_[4]) >> 1) | (static_cast<int16_t>(rx_data_[5]) << 7)) & 0x07FF);
+    data_.s1 = static_cast<Switch>(((rx_data_[5] >> 4) & 0x000C) >> 2);
+    data_.s2 = static_cast<Switch>((rx_data_[5] >> 4) & 0x0003);
     data_.mouse_x = static_cast<int16_t>(rx_data_[6]) | (static_cast<int16_t>(rx_data_[7]) << 8);
     data_.mouse_y = static_cast<int16_t>(rx_data_[8]) | (static_cast<int16_t>(rx_data_[9]) << 8);
     data_.mouse_z = static_cast<int16_t>(rx_data_[10]) | (static_cast<int16_t>(rx_data_[11]) << 8);
@@ -36,6 +41,11 @@ void RemoteController::handle(uint16_t Size) {
     HAL_UARTEx_ReceiveToIdle_DMA(huart_, rx_dma_buffer_, RX_DMA_BUFFER_SIZE);
 }
 
-bool& RemoteController::connect_state() {
-    return isConnected;
+bool RemoteController::connect_state() {
+    if (HAL_GetTick() - last_rx_tick_ < TIMEOUT) {
+        isConnected_ = true;
+    }else {
+        isConnected_ = false;
+    }
+    return isConnected_;
 }
